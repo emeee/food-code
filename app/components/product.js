030772ebb635caf44fbe0a6e5ab8fabf0d91d90d
Emiliano <Emi@1337.local>
2017-08-05 18:57:06 -0300

    Simple product screen

--- app/components/product.js
+++ app/components/product.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 export default class Product extends Component {
@@ -10,11 +10,40 @@ export default class Product extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Test foodcode</Text>
+      <View style={styles.container}>
+        <View>
+          <Text style={{fontWeight: 'bold', fontSize: 30, textAlign: 'center'}}>
+              Don Satur
+          </Text>
+          <Image
+            source={require('../images/product-1.jpg')}
+          />
+        </View>
+        <View style={styles.labels}>
+          <Image
+            source={require('../images/label-1.png')}
+          />
+          <Image
+            source={require('../images/label-2.png')}
+          />
+          <Image
+            source={require('../images/label-3.png')}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+  },
+  labels: {
+    flexDirection: 'row',
+  }
+});
 
 AppRegistry.registerComponent('product', () => product);
